8c79aadf5bbf37f00e83117c0c55ff169036f870
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-02-18 22:56:14 +0100

    Bump version 0.3

--- make-tar.sh
+++ make-tar.sh
@@ -14,6 +14,7 @@ chmod 777 .layer/fusion/s3
 # make the layer tar
 rm -f $LAYER_TAR*
 tar --preserve-permissions --owner=0 --group=0 -vcf $LAYER_TAR -C $LATER_DIR $(ls $LATER_DIR)
+mkdir -p $(dirname $LAYER_TAR)
 CHECKSUM_TAR=$(< $LAYER_TAR sha256sum | awk '{ print $1 }')
 
 # compress the TAR file, not `-n` to prevent adding timestamp
