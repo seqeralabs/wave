e1cf11c8e6e9165c9b5a713d146fa5ad9c133fb9
Jordi Deu-Pons <jordi@jordeu.net>
2022-03-23 09:47:13 +0100

    Add a timeout after mounting FusionFS, and fail with an error message

--- .layer/opt/fusion/entry.sh
+++ .layer/opt/fusion/entry.sh
@@ -7,7 +7,7 @@ if [ "$NXF_FUSION_BUCKETS" ]; then
   #export AWS_MAX_ATTEMPTS={{aws_max_attempts}}
   mkdir -p /fusion/s3
   /opt/fusion/fusionfs &
-  until (cat /proc/mounts | grep "/fusion/s3"); do sleep 1; done >/dev/null
+  timeout 10s sh -c 'until (cat /proc/mounts | grep "/fusion/s3"); do sleep 1; done >/dev/null' || { echo "ERROR mounting FusionFS at /fusion/s3"; exit 1; }
 fi
 ## invoke the target command
 if [ "$XREG_ENTRY_CHAIN" ]; then
