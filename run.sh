4921ef5c791767fb994bab928c4433bb40f018b1
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-03-27 11:25:14 +0200

    update

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 export XREG_ARCH=$(uname -m)
-#export _JAVA_OPTIONS=-Djdk.httpclient.HttpClient.log=errors,requests,headers
-./gradlew run 
+# create config file from env var
+< application-prod-example.yml \
+  DOCKER_USER=pditommaso \
+  DOCKER_PAT=${DOCKER_PAT} \
+  QUAY_USER=pditommaso \
+  QUAY_PAT=${QUAY_PAT} \
+  envsubst > application-dev.yml
+export MICRONAUT_CONFIG_FILES="classpath:application.yml,file:application-dev.yml"
+
+./gradlew run
