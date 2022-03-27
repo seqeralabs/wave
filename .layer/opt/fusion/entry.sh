63621d1874616547b7abaea2e4f67258461c4ce9
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-03-27 18:46:20 +0200

    Add trap to entry point [ci skip]

--- .layer/opt/fusion/entry.sh
+++ .layer/opt/fusion/entry.sh
@@ -15,9 +15,19 @@ if [ "$NXF_FUSION_BUCKETS" ]; then
     /opt/goofys/goofys --file-mode=0755 --uid $uid --gid $gid $bucket /fusion/s3/$path
   done)
 fi
+## make sure to shutdown the fuse driver
+on_exit() {
+  if pidof goofys >/dev/null; then
+  { echo "Shutdown goofys"
+    kill $(pidof goofys)
+    echo "Done"
+  }>&2
+  fi
+}
+trap on_exit EXIT
 ## invoke the target command
 if [ "$XREG_ENTRY_CHAIN" ]; then
-  exec "$XREG_ENTRY_CHAIN" "$@"
+  "$XREG_ENTRY_CHAIN" "$@"
 else
-  exec "$@"
+  "$@"
 fi
