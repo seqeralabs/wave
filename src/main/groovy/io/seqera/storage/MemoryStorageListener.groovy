7a91ffc919f6e60a50c4c86d0260b269389edfb4
Jorge Aguilera <jorge.aguilera@seqera.io>
2022-04-04 15:31:56 +0200

    bugfix: memory leaks with DigestByteArray
    upgrade caffeine library for removal
    use right annotation in memory storage
    generate immutable id for legacy container
    
    closes #41
    closes #57

---
+++ src/main/groovy/io/seqera/storage/MemoryStorageListener.groovy
@@ -0,0 +1,23 @@
+package io.seqera.storage
+
+import com.github.benmanes.caffeine.cache.RemovalCause
+import groovy.util.logging.Slf4j
+import com.github.benmanes.caffeine.cache.RemovalListener
+import jakarta.inject.Singleton
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * @author : jorge <jorge.aguilera@seqera.io>
+ * */
+@Slf4j
+@Singleton
+class MemoryStorageListener implements RemovalListener<String, DigestByteArray> {
+
+    private static Logger logger = LoggerFactory.getLogger(MemoryStorageListener.class);
+
+    @Override
+    void onRemoval(String key, DigestByteArray value, RemovalCause cause) {
+        logger.info("Removing $key from cache because $cause");
+    }
+}
