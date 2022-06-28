c6d6531549d987d3fe5cc3eddc4a88924becdb03
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-06-28 18:42:41 +0200

    Fix compilation issue
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/service/ContainerRegistryKeys.groovy
+++ src/main/groovy/io/seqera/service/ContainerRegistryKeys.groovy
@@ -1,18 +1,20 @@
 package io.seqera.service
 
 import groovy.json.JsonSlurper
+import groovy.transform.CompileStatic
 
 /**
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
+@CompileStatic
 class ContainerRegistryKeys {
     String userName
     String password
     String registry
 
     static ContainerRegistryKeys fromJson(String json) {
-        Map root = new JsonSlurper().parseText(json)
+        final root = (Map) new JsonSlurper().parseText(json)
         return new ContainerRegistryKeys(userName: root.userName, registry: root.registry)
     }
 }
