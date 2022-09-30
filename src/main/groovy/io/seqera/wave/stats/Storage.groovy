f661251cc6d46f5bcaabbbf4240ae78f389be381
Jorge Aguilera <jorge.aguilera@seqera.io>
2022-09-30 10:01:14 +0200

    minor changes in surrealdb implementation
    Signed-off-by: Jorge Aguilera <jorge.aguilera@seqera.io>

--- src/main/groovy/io/seqera/wave/stats/Storage.groovy
+++ src/main/groovy/io/seqera/wave/stats/Storage.groovy
@@ -1,10 +1,15 @@
 package io.seqera.wave.stats
 
+import groovy.transform.CompileStatic
+
 
 /**
+ * A storage for statistic datas
+ *
  * @author : jorge <jorge.aguilera@seqera.io>
  *
  */
+@CompileStatic
 interface Storage {
 
     void addBuild(BuildBean build)
--- src/main/groovy/io/seqera/wave/stats/surrealdb/SurrealConstants.groovy
+++ src/main/groovy/io/seqera/wave/stats/surrealdb/SurrealConstants.groovy
@@ -1,10 +1,15 @@
 package io.seqera.wave.stats.surrealdb
 
+import groovy.transform.CompileStatic
+
 
 /**
+ * Final class to share some static values
+ *
  * @author : jorge <jorge.aguilera@seqera.io>
  *
  */
+@CompileStatic
 final class SurrealConstants {
 
     public static final String BUILD_WAVE_TABLENAME = 'build_wave'
