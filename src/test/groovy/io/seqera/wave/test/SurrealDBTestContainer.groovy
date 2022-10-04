beb8c577e3d1298ca17f202b1f9b2e5bad7d945d
Jorge Aguilera <jorge.aguilera@seqera.io>
2022-10-04 06:13:36 -0500

    wait for surrealdb ready
    Signed-off-by: Jorge Aguilera <jorge.aguilera@seqera.io>

--- src/test/groovy/io/seqera/wave/test/SurrealDBTestContainer.groovy
+++ src/test/groovy/io/seqera/wave/test/SurrealDBTestContainer.groovy
@@ -3,6 +3,7 @@ package io.seqera.wave.test
 import spock.lang.Shared
 
 import org.testcontainers.containers.GenericContainer
+import org.testcontainers.containers.wait.strategy.Wait
 import org.testcontainers.utility.DockerImageName
 
 /**
@@ -15,7 +16,9 @@ trait SurrealDBTestContainer {
     static GenericContainer surrealContainer = new GenericContainer(DockerImageName.parse("surrealdb/surrealdb:latest"))
             .withExposedPorts(8000)
             .withCommand("start","--user", "root", "--pass", "root", '--log', 'debug')
-
+            .waitingFor(
+                    Wait.forLogMessage(".*Started web server on .*\\n", 1)
+            )
 
     void restartDb(){
         if( surrealContainer.running)
