0d668a0dbca020686970d393b9db1e3e00ae0174
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2022-10-27 19:24:26 +0200

    Minor
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/stats/StatsService.groovy
+++ src/main/groovy/io/seqera/wave/stats/StatsService.groovy
@@ -30,16 +30,18 @@ class StatsService implements ApplicationEventListener<BuildEvent> {
     static BuildRecord fromEvent(BuildEvent event) {
         return new BuildRecord(
                 buildId: event.buildRequest.id,
-                dockerFile:event.buildRequest.dockerFile?.replaceAll("[\ud83c\udf00-\ud83d\ude4f]|[\ud83d\ude80-\ud83d\udeff]", ""),
-                condaFile:event.buildRequest.condaFile?.replaceAll("[\ud83c\udf00-\ud83d\ude4f]|[\ud83d\ude80-\ud83d\udeff]", ""),
-                targetImage:event.buildRequest.targetImage,
-                userName:event.buildRequest.user?.userName,
-                userEmail:event.buildRequest.user?.email,
-                userId:event.buildRequest.user?.id,
-                ip:event.buildRequest.ip,
-                startTime:event.buildRequest.startTime,
-                duration:event.buildResult.duration,
-                exitStatus:event.buildResult.exitStatus,
+                // note: the string replacement is needed to a bug in the SurrealDb version 1.0.0-beta.8
+                // see https://pullanswer.com/questions/bug-unicode-escaped-characters-with-surrogate-pairs-causes-surrealdb-to-panic
+                dockerFile: event.buildRequest.dockerFile?.replaceAll("[\ud83c\udf00-\ud83d\ude4f]|[\ud83d\ude80-\ud83d\udeff]", ""),
+                condaFile: event.buildRequest.condaFile?.replaceAll("[\ud83c\udf00-\ud83d\ude4f]|[\ud83d\ude80-\ud83d\udeff]", ""),
+                targetImage: event.buildRequest.targetImage,
+                userName: event.buildRequest.user?.userName,
+                userEmail: event.buildRequest.user?.email,
+                userId: event.buildRequest.user?.id,
+                ip: event.buildRequest.ip,
+                startTime: event.buildRequest.startTime,
+                duration: event.buildResult.duration,
+                exitStatus: event.buildResult.exitStatus,
         )
     }
 }
