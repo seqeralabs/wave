4dbb439cfa22db43e251ad96ae79ebb3e743b61f
Andrea Tortorella <andrea.tortorella@seqera.io>
2023-01-17 11:26:56 +0100

    add conditional put tests and documentation

--- src/main/groovy/io/seqera/wave/service/security/SecurityService.groovy
+++ src/main/groovy/io/seqera/wave/service/security/SecurityService.groovy
@@ -3,7 +3,17 @@ package io.seqera.wave.service.security
 import io.seqera.wave.exchange.PairServiceResponse
 
 /**
- * Provides public key generation for tower credentials integration
+ * Provides public key generation for tower credentials integration.
+ *
+ * Once {@link SecurityService#getPublicKey(java.lang.String, java.lang.String)} is
+ * called a new {@link KeyRecord} for the requested service is generated and cached until it expires.
+ *
+ * Further invocation of {@link SecurityService#getPublicKey(java.lang.String, java.lang.String)}
+ * will not generate a new {@code KeyRecord} and return instead the public side of the already
+ * generated one.
+ *
+ * Access to the currently generated {@code KeyRecord} for the corresponding service is provided
+ * through {@link SecurityService#getServiceRegistration(java.lang.String, java.lang.String)}
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
@@ -11,8 +21,28 @@ interface SecurityService {
 
     public static String TOWER_SERVICE = "tower"
 
+
+    /**
+     * Generates an return a key pair for the provided {@code service} available
+     * at {@code endpoint}
+     *
+     * The key-pair is generated only if it is not already available for (service,endpoint)
+     * otherwise the current key is returned.
+     *
+     * @param service The service name
+     * @param endpoint The endpoint of the service
+     * @return {@link PairServiceResponse} with the generated encoded public key
+     */
     PairServiceResponse getPublicKey(String service, String endpoint)
 
+    /**
+     * Get the {@link KeyRecord} associated with {@code service} and {@code endpoint}
+     * generated with {@link #getPublicKey(java.lang.String, java.lang.String)}
+     *
+     * @param service The service name
+     * @param endpoint The endpoint of the service
+     * @return {@link KeyRecord} if it has been generated and not expired, {@code null} otherwise
+     */
     KeyRecord getServiceRegistration(String service, String endpoint)
     
 }
---
+++ src/test/groovy/io/seqera/wave/service/cache/impl/LocalCacheProviderTest.groovy
@@ -0,0 +1,59 @@
+package io.seqera.wave.service.cache.impl
+
+import spock.lang.Specification
+
+import java.time.Duration
+
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import jakarta.inject.Inject
+
+
+@MicronautTest(environments = ['test'])
+class LocalCacheProviderTest extends Specification {
+
+    @Inject
+    LocalCacheProvider localCacheProvider
+
+
+    def 'conditional put with current value when ke is not set'() {
+        when: 'conditionally set a key that has no current value'
+        def current = localCacheProvider.putIfAbsentAndGetCurrent('key', 'new-value', Duration.ofMillis(Long.MAX_VALUE))
+
+        then: 'the provided value is returned'
+        current == 'new-value'
+
+        and: 'the value is set in the store'
+        localCacheProvider.get('key') == 'new-value'
+    }
+
+    def 'conditional put with current value when key is already set'() {
+        given: 'a store containing a mapping for key that is not expired'
+        localCacheProvider.put('key','existing', Duration.ofMillis(Long.MAX_VALUE))
+
+        when: 'try to conditionally set the key to a new value'
+        def current = localCacheProvider.putIfAbsentAndGetCurrent('key', 'new-value', Duration.ofMillis(Long.MAX_VALUE))
+
+        then: 'the existing value is returned'
+        current == 'existing'
+
+        and: 'the value is not updated in the store'
+        localCacheProvider.get('key') == 'existing'
+    }
+
+
+    def 'conditional put with current value when key is set and has expired'() {
+        given: 'a store containing a mapping for key that will expire'
+        localCacheProvider.put('key', 'existing', Duration.ofMillis(100))
+        // give time for cache store to expire the key
+        sleep(Duration.ofMillis(200).toMillis())
+
+        when: 'try to conditionally set the key to a new value'
+        def current = localCacheProvider.putIfAbsentAndGetCurrent('key', 'new-value', Duration.ofMillis(100))
+
+        then: 'the provided value is returned'
+        current == 'new-value'
+
+        and: 'the value is updated is set in the store'
+        localCacheProvider.get('key') == 'new-value'
+    }
+}
