ede9a242f84c7849efbaab65667883e8ec339b8b
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-02-15 20:05:36 +0100

    Add landing redirection
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/controller/ServiceInfoController.groovy
+++ src/main/groovy/io/seqera/wave/controller/ServiceInfoController.groovy
@@ -1,13 +1,15 @@
 package io.seqera.wave.controller
 
+import javax.annotation.Nullable
+
 import groovy.util.logging.Slf4j
+import io.micronaut.context.annotation.Value
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.seqera.wave.api.ServiceInfo
 import io.seqera.wave.api.ServiceInfoResponse
 import io.seqera.wave.util.BuildInfo
-
 /**
  * Basic service info endpoint
  *
@@ -17,12 +19,23 @@ import io.seqera.wave.util.BuildInfo
 @Controller("/")
 class ServiceInfoController {
 
+    @Value('${wave.landing.url}')
+    @Nullable
+    String landingUrl
+
     @Get('/service-info')
     HttpResponse<ServiceInfoResponse> info() {
         final info = new ServiceInfo()
         info.version = BuildInfo.getVersion()
         info.commitId = BuildInfo.getCommitId()
         HttpResponse.ok(new ServiceInfoResponse(info))
+    }
+
+    @Get("/")
+    HttpResponse landing() {
+        return landingUrl
+                ? HttpResponse.redirect(new URI(landingUrl))
+                : HttpResponse.badRequest()
     }
 
 }
