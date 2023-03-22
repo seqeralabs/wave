de07254e47a136a18fa33a18d8a3a8e7dd10924f
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-03-22 09:33:49 +0100

    Fix digest store checksum compute
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/RegHelperTest.groovy
+++ src/test/groovy/io/seqera/wave/RegHelperTest.groovy
@@ -1,17 +1,18 @@
 package io.seqera.wave
 
-import spock.lang.Unroll
-
-import io.seqera.wave.test.ManifestConst
-import io.seqera.wave.util.RegHelper
 import spock.lang.Specification
 
-import io.seqera.wave.util.ZipUtils
+import java.util.concurrent.CompletableFuture
 
+import groovy.util.logging.Slf4j
+import io.seqera.wave.test.ManifestConst
+import io.seqera.wave.util.RegHelper
+import io.seqera.wave.util.ZipUtils
 /**
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
+@Slf4j
 class RegHelperTest extends Specification {
 
     def 'should compute digest' () {
@@ -59,27 +60,39 @@ class RegHelperTest extends Specification {
         RegHelper.random256Hex().length() == 64
     }
 
-    @Unroll("Validate digest #i time")
     def 'should validate digest generation' () {
         given:
-        def len = new Random().nextInt(100,1_000)
-        def TEXT = RegHelper.random256Hex() * len * i
-        def digest = RegHelper.digest(TEXT)
+        def futures = new ArrayList<>()
 
         when:
-        def buffer = ZipUtils.compress(TEXT)
+        for( int i=0; i< 20; i++ )  {
+            def fut = CompletableFuture.supplyAsync {
+                log.debug "Check starting"
+                def times = new Random().nextInt(20)
+                def len = new Random().nextInt(100,1_000)
+                def TEXT = RegHelper.random256Hex() * len * times
+                def digest = RegHelper.digest(TEXT)
+
+                when:
+                def buffer = ZipUtils.compress(TEXT)
+                then:
+                def c1 = ZipUtils.decompressAsString(buffer)
+                def c2 = ZipUtils.decompressAsBytes(buffer)
+                and:
+                assert c1 == TEXT
+                assert RegHelper.digest(c1) == digest
+                and:
+                assert c2 == TEXT.bytes
+                assert RegHelper.digest(new String(c2)) == digest
+                log.debug "Check done"
+            }
+            futures.add(fut)
+        }
+
+        and:
+        CompletableFuture.allOf(futures as CompletableFuture[]).get()
+
         then:
-        def c1 = ZipUtils.decompressAsString(buffer)
-        def c2 = ZipUtils.decompressAsBytes(buffer)
-        and:
-        c1 == TEXT
-        RegHelper.digest(c1) == digest
-        and:
-        c2 == TEXT.bytes
-        RegHelper.digest(new String(c2)) == digest
-
-        where:
-        i << (1..100)
-
+        true
     }
 }
