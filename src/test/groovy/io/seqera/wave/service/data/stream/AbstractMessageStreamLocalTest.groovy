cfaa9dd09f006928535665840773aa284d80bc74
Jordi Deu-Pons <jordi@jordeu.net>
2023-03-30 09:05:57 +0200

    Add stream unit tests
    Signed-off-by: Jordi Deu-Pons <jordi@jordeu.net>

---
+++ src/test/groovy/io/seqera/wave/service/data/stream/AbstractMessageStreamLocalTest.groovy
@@ -0,0 +1,70 @@
+package io.seqera.wave.service.data.stream
+
+import spock.lang.Specification
+
+import groovy.transform.Canonical
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.seqera.wave.service.data.stream.impl.LocalMessageBroker
+import jakarta.inject.Inject
+
+/**
+ * Test class {@link AbstractMessageStream} using a {@link LocalMessageBroker}
+ *
+ * @author Jordi Deu-Pons <jordi@seqera.io>
+ */
+@MicronautTest(environments = ['test'])
+class AbstractMessageStreamLocalTest extends Specification {
+
+    @Canonical
+    static class Simple {
+        String value
+    }
+
+    static class SimpleDataStream extends AbstractMessageStream<Simple> {
+
+        SimpleDataStream(MessageBroker<String> broker) {
+            super(broker)
+        }
+
+        @Override
+        String getPrefix() {
+            return "local-test"
+        }
+    }
+
+    @Inject
+    private MessageBroker<String> broker
+
+
+    def 'should register and unregister consumers'() {
+        given:
+        def stream = new SimpleDataStream(broker)
+
+        when:
+        stream.registerConsumer('service-key', 'consumer-id', {})
+        then:
+        stream.hasConsumer('service-key')
+
+        when:
+        stream.unregisterConsumer('service-key', 'consumer-id')
+        then:
+        !stream.hasConsumer('service-key')
+
+    }
+
+    def 'should send and consume a request'() {
+        given:
+        def stream = new SimpleDataStream(broker)
+        def result = null
+
+        when:
+        stream.registerConsumer('service-key', 'consumer-id', { result = it })
+        and:
+        stream.sendMessage('service-key', new Simple('hello'))
+        then:
+        result.value == 'hello'
+
+    }
+
+
+}
---
+++ src/test/groovy/io/seqera/wave/service/data/stream/AbstractMessageStreamRedisTest.groovy
@@ -0,0 +1,108 @@
+package io.seqera.wave.service.data.stream
+
+import spock.lang.Shared
+import spock.lang.Specification
+
+import groovy.transform.Canonical
+import io.micronaut.context.ApplicationContext
+import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.seqera.wave.service.data.stream.impl.RedisMessageBroker
+import io.seqera.wave.test.RedisTestContainer
+
+/**
+ * Test class {@link AbstractMessageStream} using a {@link RedisMessageBroker}
+ *
+ * @author Jordi Deu-Pons <jordi@seqera.io>
+ */
+@MicronautTest(environments = ['test'])
+class AbstractMessageStreamRedisTest extends Specification implements RedisTestContainer {
+
+    @Shared
+    ApplicationContext applicationContext
+
+    def setup() {
+        applicationContext = ApplicationContext.run([
+                REDIS_HOST: redisHostName,
+                REDIS_PORT: redisPort
+        ], 'test', 'redis')
+
+    }
+
+    @Canonical
+    static class Simple {
+        String value
+    }
+
+    static class SimpleDataStream extends AbstractMessageStream<Simple> {
+
+        SimpleDataStream(MessageBroker<String> broker) {
+            super(broker)
+        }
+
+        @Override
+        String getPrefix() {
+            return "local-test"
+        }
+    }
+
+    def 'should register and unregister consumers'() {
+        given:
+        def broker = applicationContext.getBean(RedisMessageBroker)
+        def stream = new SimpleDataStream(broker)
+
+        when:
+        stream.registerConsumer('service-key-one', 'consumer-id', {})
+        and:
+        sleep(500)
+        then:
+        stream.hasConsumer('service-key-one')
+
+        when:
+        stream.unregisterConsumer('service-key-one', 'consumer-id')
+        and:
+        sleep(500)
+        then:
+        !stream.hasConsumer('service-key-one')
+
+    }
+
+    def 'should send and consume a request'() {
+        given:
+        def broker = applicationContext.getBean(RedisMessageBroker)
+        def stream = new SimpleDataStream(broker)
+        def result = null
+
+        when:
+        stream.registerConsumer('service-key-two', 'consumer-id', { result = it })
+        and:
+        stream.sendMessage('service-key-two', new Simple('hello'))
+        and:
+        sleep(500)
+        then:
+        result.value == 'hello'
+
+    }
+
+    def 'should send and consume a request across instances'() {
+        given:
+        def broker1 = applicationContext.getBean(RedisMessageBroker)
+        def stream1 = new SimpleDataStream(broker1)
+        and:
+        def broker2 = applicationContext.getBean(RedisMessageBroker)
+        def stream2 = new SimpleDataStream(broker2)
+        and:
+        def result = null
+
+        when:
+        stream2.registerConsumer('service-key-three', 'consumer-id', { result = it })
+        and:
+        stream1.sendMessage('service-key-three', new Simple('hello'))
+        and:
+        sleep(500)
+        then:
+        result.value == 'hello'
+
+    }
+
+
+}
