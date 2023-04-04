a9d640a01e13a68ccc8b980ab5a1f8f7f281c928
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-04-04 18:59:57 +0200

    Fix missing timeout exeception on redis future
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/service/data/future/impl/RedisFutureQueue.groovy
+++ src/main/groovy/io/seqera/wave/service/data/future/impl/RedisFutureQueue.groovy
@@ -39,15 +39,11 @@ class RedisFutureQueue implements FutureQueue<String>  {
             try {
                 final result = conn.brpop(timeout.toSeconds() as int , key)
                 if( !result )
-                    return null
-                if( result && result.size()==2 ) {
-                    log.trace "Received queue message=$result"
-                    return result[1]
-                }
-                else {
-                    log.error "Unexpected length for queue message=$result"
-                    return result[1]
-                }
+                    throw new TimeoutException("Unable to retrieve a value for key: $key")
+                if( result.size()!=2 )
+                    throw new IllegalStateException("Unexpected length for future message: $result")
+                log.trace "Received future message=$result"
+                return result[1]
             }
             finally {
                 conn.del(key)
