642131bbcd389cfedc4767518f65deca948b6c2f
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-06-04 21:49:49 +0200

    Fix BuildInfo class name
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/util/BuildInfoTest.groovy
+++ src/test/groovy/io/seqera/wave/util/BuildInfoTest.groovy
@@ -1,14 +1,11 @@
 package io.seqera.wave.util
 
-import io.seqera.wave.util.BuildInfo
 import spock.lang.Specification
-
 /**
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
-class BuildIntoTest extends Specification {
-
+class BuildInfoTest extends Specification {
 
     def 'should load version and commit id' () {
         expect:
