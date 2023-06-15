434ab37473db492cd7d6fcfc3e1a76b1051105b4
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-06-15 20:40:36 +0200

    Fix failing test
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/ProxyClientTest.groovy
+++ src/test/groovy/io/seqera/wave/ProxyClientTest.groovy
@@ -142,7 +142,7 @@ class ProxyClientTest extends Specification {
     @Requires({System.getenv('AWS_ACCESS_KEY_ID') && System.getenv('AWS_SECRET_ACCESS_KEY')})
     def 'should call target manifest on ecr public' () {
         given:
-        def IMAGE = 'seqera-labs/nf-jdk'
+        def IMAGE = 'seqera-labs/nextflow'
         def REG = 'public.ecr.aws'
         def registry = lookupService.lookup(REG)
         def creds = credentialsProvider.getDefaultCredentials(REG)
@@ -154,7 +154,7 @@ class ProxyClientTest extends Specification {
                 .withCredentials(creds)
 
         when:
-        def resp = proxy.getString("/v2/$IMAGE/manifests/corretto-17.0.5")
+        def resp = proxy.getString("/v2/$IMAGE/manifests/23.04.0")
         then:
         resp.statusCode() == 200
     }
