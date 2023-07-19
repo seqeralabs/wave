8cbe3944c20c457001990ca791af4516f27b390a
munish chouhan <hrma017@gmail.com>
2023-07-19 18:33:32 +0200

    changes made as per review

--- src/main/groovy/io/seqera/wave/service/aws/AwsMailProvider.groovy
+++ src/main/groovy/io/seqera/wave/service/aws/AwsMailProvider.groovy
@@ -4,7 +4,10 @@ import java.nio.ByteBuffer
 import javax.mail.internet.MimeMessage
 
 import com.amazonaws.AmazonClientException
+import com.amazonaws.AmazonWebServiceClient
 import com.amazonaws.regions.InstanceMetadataRegionProvider
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailServiceClientBuilder
 import com.amazonaws.services.simpleemail.model.RawMessage
 import com.amazonaws.services.simpleemail.model.SendRawEmailRequest
 import groovy.transform.CompileStatic
@@ -30,8 +33,13 @@ import jakarta.inject.Singleton
 @Slf4j
 class AwsMailProvider implements MailProvider {
 
-    @Inject
-    private AwsClientFactory clientFactory
+    private AmazonSimpleEmailService amazonSimpleEmailService
+
+    AwsMailProvider() {
+        amazonSimpleEmailService = AmazonSimpleEmailServiceClientBuilder
+                .standard()
+                .build()
+    }
 
     @PostConstruct
     private void init() {
@@ -39,35 +47,12 @@ class AwsMailProvider implements MailProvider {
     }
 
     void send(MimeMessage message, Mailer mailer) {
-        final region = fetchRegion()
-        final client = clientFactory.getEmailClient()
         // dump the message to a buffer
         final outputStream = new ByteArrayOutputStream()
         message.writeTo(outputStream)
         // send the email
         final rawMessage = new RawMessage(ByteBuffer.wrap(outputStream.toByteArray()))
-        final result = client.sendRawEmail(new SendRawEmailRequest(rawMessage));
+        final result = amazonSimpleEmailService.sendRawEmail(new SendRawEmailRequest(rawMessage));
         log.debug "Mail message sent: ${result}"
     }
-
-    /**
-     * Retrieve the AWS region from the EC2 instance metadata.
-     * See http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/ec2-instance-metadata.html
-     *
-     * @return
-     *      The AWS region of the current EC2 instance eg. {@code eu-west-1} or
-     *      {@code null} if it's not an EC2 instance.
-     */
-    private String fetchRegion() {
-        try {
-            return System.getenv('AWS_REGION')
-                    ?: System.getenv('AWS_DEFAULT_REGION')
-                    ?: new InstanceMetadataRegionProvider().getRegion()
-        }
-        catch (AmazonClientException e) {
-            log.error "Enable to retrieve AWS region - make sure either AWS_REGION or AWS_DEFAULT_REGION variable is defined or to run into a AWS Ec2 instance", e
-            return null
-        }
-    }
-
 }
