6c36063a5843223dc6e6200eb837e865668182ac
munishchouhan <hrma017@gmail.com>
2023-08-16 07:13:20 +0200

    refactor

--- wave-utils/src/main/java/io/seqera/wave/util/PathFilter.java
+++ wave-utils/src/main/java/io/seqera/wave/util/PathFilter.java
@@ -1,8 +1,12 @@
 package io.seqera.wave.util;
 
+import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.util.HashMap;
 import java.util.LinkedHashSet;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -16,22 +20,33 @@ public class PathFilter {
 
     private final String EXCEPTION_PATTERN_MARKER = "!";
 
+    Map<String, PathMatcher> mactherMap = new HashMap<>();
+
     public PathFilter(LinkedHashSet<String> patterns) {
         this.patterns = patterns;
+        FileSystem fileSystem = FileSystems.getDefault();
+        for (String pattern : patterns){
+            if (pattern.startsWith(EXCEPTION_PATTERN_MARKER)){
+                mactherMap.put(pattern,fileSystem.getPathMatcher("glob:" + pattern.substring(1)));
+            }else{
+                mactherMap.put(pattern,fileSystem.getPathMatcher("glob:" + pattern));
+            }
+        }
     }
 
     public boolean accept(Path path) {
         boolean accepted = true;
+
         for (String pattern : patterns) {
-            System.out.println(path);
-            if (pattern.startsWith(EXCEPTION_PATTERN_MARKER)) {
-                if (FileSystems.getDefault().getPathMatcher("glob:" + pattern.substring(1)).matches(path)) {
+            if (mactherMap.get(pattern).matches(path)) {
+                if (pattern.startsWith(EXCEPTION_PATTERN_MARKER)) {
                     accepted = true;
+                } else {
+                    accepted = false;
                 }
-            } else if (FileSystems.getDefault().getPathMatcher("glob:" + pattern).matches(path)) {
-                accepted = false;
             }
         }
+
         return accepted;
     }
 }
