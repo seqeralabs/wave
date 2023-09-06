c5c16b89fca733a411db6f8c24568834827576a7
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-09-06 14:44:22 +0200

    Bump MPL 2.0 license
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- docs.sh
+++ docs.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+#
+#  Copyright (c) 2023, Seqera Labs.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  This Source Code Form is "Incompatible With Secondary Licenses", as
+#  defined by the Mozilla Public License, v. 2.0.
+#
+
 action=${1:-build}
 if [[ $action = 'serve' ]]; then
   docker run --rm -it -p 8001:8000 -v ${PWD}:/docs squidfunk/mkdocs-material
