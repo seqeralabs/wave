8f4677d4468830ee3630a0c8e0b8a72007719d77
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-09-06 14:44:22 +0200

    Bump MPL 2.0 license
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- debug.sh
+++ debug.sh
@@ -1,2 +1,13 @@
+#
+#  Copyright (c) 2023, Seqera Labs.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  This Source Code Form is "Incompatible With Secondary Licenses", as
+#  defined by the Mozilla Public License, v. 2.0.
+#
+
 export JVM_OPTS='-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=8010'
 . ./run.sh
--- src/main/jib/launch.sh
+++ src/main/jib/launch.sh
@@ -1,3 +1,14 @@
+#
+#  Copyright (c) 2023, Seqera Labs.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+#  This Source Code Form is "Incompatible With Secondary Licenses", as
+#  defined by the Mozilla Public License, v. 2.0.
+#
+
 # Launch backend server
 [ "$WAVE_JVM_OPTS" ] && echo "Detected WAVE_JVM_OPTS=$WAVE_JVM_OPTS"
 exec java \
