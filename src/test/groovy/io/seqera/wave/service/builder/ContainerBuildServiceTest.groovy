a1971e436d9af528d5437711ddf8b49c099fe781
munishchouhan <hrma017@gmail.com>
2023-09-18 13:54:09 +0200

    added 2>/dev/null in  singularity file

--- src/test/groovy/io/seqera/wave/service/builder/ContainerBuildServiceTest.groovy
+++ src/test/groovy/io/seqera/wave/service/builder/ContainerBuildServiceTest.groovy
@@ -285,7 +285,7 @@ class ContainerBuildServiceTest extends Specification {
                 'Stage: build')
         result.contains('spack config add packages:all:target:[x86_64]')
         result.contains('spack mirror add seqera-spack s3://bucket/cache')
-        result.contains('fingerprint="$(spack gpg trust /mnt/key | sed -nr "s/^gpg: key ([0-9A-F]{16}): secret key imported$/\\1/p")"')
+        result.contains('fingerprint="$(spack gpg trust /mnt/key 2>/dev/null| sed -nr "s/^gpg: key ([0-9A-F]{16}): secret key imported$/\\1/p")"')
         result.contains('/some/context/dir/spack.yaml /opt/spack-env/spack.yaml')
 
         cleanup:
