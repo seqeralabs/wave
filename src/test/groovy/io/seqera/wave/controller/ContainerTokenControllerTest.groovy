e95eb371a546d14cd9cfeb076820b1f2586efaf5
munishchouhan <hrma017@gmail.com>
2023-09-19 13:02:26 +0200

    Added AWS env to Dockerfile

--- src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
+++ src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
@@ -249,12 +249,12 @@ class ContainerTokenControllerTest extends Specification {
         submit = new SubmitContainerTokenRequest(containerFile: encode('FROM foo'), spackFile: encode('some::spack-recipe'), containerPlatform: 'arm64')
         build = controller.makeBuildRequest(submit, null, "")
         then:
-        build.id == '606e1d357ba24cd0'
+        build.id == '1117d9bad92246ef'
         build.containerFile.endsWith('\nFROM foo')
         build.containerFile.startsWith('# Builder image\n')
         build.condaFile == null
         build.spackFile == 'some::spack-recipe'
-        build.targetImage == 'wave/build:606e1d357ba24cd0'
+        build.targetImage == 'wave/build:1117d9bad92246ef'
         build.workDir == Path.of('/some/wsp').resolve(build.id)
         build.platform == ContainerPlatform.of('arm64')
     }
