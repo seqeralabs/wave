6661f5656ab165ac290c2b5c28d4e02db7f44d87
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-09-25 10:14:14 +0200

    Fix spack build for singularity [release]
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
+++ src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
@@ -249,12 +249,12 @@ class ContainerTokenControllerTest extends Specification {
         submit = new SubmitContainerTokenRequest(containerFile: encode('FROM foo'), spackFile: encode('some::spack-recipe'), containerPlatform: 'arm64')
         build = controller.makeBuildRequest(submit, null, "")
         then:
-        build.id == '40d89e9bae63df13'
+        build.id == '99e29216d3b26158'
         build.containerFile.endsWith('\nFROM foo')
         build.containerFile.startsWith('# Builder image\n')
         build.condaFile == null
         build.spackFile == 'some::spack-recipe'
-        build.targetImage == 'wave/build:40d89e9bae63df13'
+        build.targetImage == 'wave/build:99e29216d3b26158'
         build.workDir == Path.of('/some/wsp').resolve(build.id)
         build.platform == ContainerPlatform.of('arm64')
     }
