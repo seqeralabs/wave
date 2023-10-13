a16f40a8b50de4275b3e702d1d88e1cfaca15bef
munishchouhan <hrma017@gmail.com>
2023-10-13 15:01:53 +0200

    Lic added

--- src/test/groovy/io/seqera/wave/filter/DenyPathsFilterTest.groovy
+++ src/test/groovy/io/seqera/wave/filter/DenyPathsFilterTest.groovy
@@ -1,9 +1,32 @@
+/*
+ *  Wave, containers provisioning service
+ *  Copyright (c) 2023, Seqera Labs
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.seqera.wave.filter
 
 import spock.lang.Specification
 
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
 import jakarta.inject.Inject
+
+/**
+ *
+ * @author Munish Chouhan <munish.chouhan@seqera.io>
+ */
 
 @MicronautTest
 class DenyPathsFilterTest extends Specification {
