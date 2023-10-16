a7f52ed6755b215da1ae6302bc8068e731d322b8
munishchouhan <hrma017@gmail.com>
2023-10-16 14:14:49 +0200

    Fixed UT

--- src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
+++ src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
@@ -256,12 +256,12 @@ class ContainerTokenControllerTest extends Specification {
         submit = new SubmitContainerTokenRequest(containerFile: encode('FROM foo'), spackFile: encode('some::spack-recipe'), containerPlatform: 'arm64')
         build = controller.makeBuildRequest(submit, null, "")
         then:
-        build.id == 'cae09ac04a4ade04'
+        build.id == '305f7f871d02e3d1'
         build.containerFile.endsWith('\nFROM foo')
         build.containerFile.startsWith('# Builder image\n')
         build.condaFile == null
         build.spackFile == 'some::spack-recipe'
-        build.targetImage == 'wave/build:cae09ac04a4ade04'
+        build.targetImage == 'wave/build:305f7f871d02e3d1'
         build.workDir == Path.of('/some/wsp').resolve(build.id)
         build.platform == ContainerPlatform.of('arm64')
     }
