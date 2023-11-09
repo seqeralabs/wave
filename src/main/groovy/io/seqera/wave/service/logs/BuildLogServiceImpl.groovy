6885a015c036046855c179691d95f3ce1b6ae0c3
munishchouhan <hrma017@gmail.com>
2023-11-09 09:07:41 +0100

    build success condition removed from store log to s3 bucket

--- src/main/groovy/io/seqera/wave/service/logs/BuildLogServiceImpl.groovy
+++ src/main/groovy/io/seqera/wave/service/logs/BuildLogServiceImpl.groovy
@@ -81,10 +81,7 @@ class BuildLogServiceImpl implements BuildLogService {
 
     @EventListener
     void onBuildEvent(BuildEvent event) {
-        if( event.result.succeeded() ) {
-            final buildId = event.result.id
-            CompletableFuture.supplyAsync(()-> storeLog(buildId, event.result.logs))
-        }
+            CompletableFuture.supplyAsync(()-> storeLog(event.result.id, event.result.logs))
     }
 
     @Override
