1a90cbc2f5f1f855fa8b6ea8c937cb01538c96b4
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-11-24 14:12:51 +0100

    Fix Container 302 redirect response with body
    This commit fixes the handling of redirect response made by
    some registry e.g. Quay.io which can provide a repsonse body
    other than the Location header directive.
    
    In this situation Wave was discarding the response body causing
    the client to not close correcly the connection because it was
    awaiting the payload as declared by the Length header.
    
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/controller/RegistryProxyController.groovy
+++ src/main/groovy/io/seqera/wave/controller/RegistryProxyController.groovy
@@ -245,11 +245,11 @@ class RegistryProxyController {
         if( resp.isRedirect() ) {
             final loc = log.isTraceEnabled() ? resp.location : stripUriParams(resp.location)
             log.debug "Forwarding ${route.type} request '${route.getTargetContainer()}' to '${loc}'"
-            return fromRedirectResponse(resp)
+            return fromRedirectResponse(resp, route)
         }
         else if( route.isManifest() ) {
             log.debug "Pulling manifest from repository: '${route.getTargetContainer()}'"
-            return fromManifestResponse(resp)
+            return fromManifestResponse(resp, route)
         }
         else {
             log.debug "Pulling blob from repository: '${route.getTargetContainer()}'"
@@ -327,12 +327,18 @@ class RegistryProxyController {
                 .headers(headers)
     }
 
-    MutableHttpResponse<?> fromRedirectResponse(final DelegateResponse resp) {
+    MutableHttpResponse<?> fromRedirectResponse(final DelegateResponse resp, RoutePath route) {
+        // create the retry logic on error                                                              §
+        final retryable = Retryable
+                .<byte[]>of(httpConfig)
+                .onRetry((event) -> log.warn("Unable to read redirect body - request: $route; event: $event"))
+
         final override = Map.of(
                     'Location', resp.location,  // <-- the location can be relative to the origin host, override it to always return a fully qualified URI
                     'Connection', 'close' ) // <-- make sure to return connection: close header otherwise docker hangs
         return HttpResponse
                 .status(HttpStatus.valueOf(resp.statusCode))
+                .body(retryable.apply(()-> resp.body.bytes))
                 .headers(toMutableHeaders(resp.headers, override))
     }
 
@@ -352,10 +358,15 @@ class RegistryProxyController {
                 .headers(toMutableHeaders(response.headers))
     }
 
-    MutableHttpResponse<?> fromManifestResponse(DelegateResponse resp) {
+    MutableHttpResponse<?> fromManifestResponse(DelegateResponse resp, RoutePath route) {
+        // create the retry logic on error                                                              §
+        final retryable = Retryable
+                .<byte[]>of(httpConfig)
+                .onRetry((event) -> log.warn("Unable to read manifest body - request: $route; event: $event"))
+
         HttpResponse
                 .status(HttpStatus.valueOf(resp.statusCode))
-                .body(resp.body.bytes)
+                .body(retryable.apply(()-> resp.body.bytes))
                 .headers(toMutableHeaders(resp.headers))
     }
 
