397b18d2389ad297374732485acf793eed3217f3
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2023-11-25 10:41:44 +0100

    Use virtual threads for I/O bound operations
    This commit introduces the use of virtual threads for short-lived
    I/O bound operations. More in details virtual threads are used in
    place of the usual thread pool for:
    - PairingInboundStore: use a virtual thread pool to creare a CompletableFuture
      to await the completation of a remote command
    - HttpClientFactory: use a virtual thread for HttpClient operations
    - TimedInputStream: use a virtual thread pool to read the target input
      stream within a fixed amount of time.
    
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>
    Signed-off-by: Dr Marco Claudio De La Pierre <marco.delapierre@gmail.com>

--- src/main/groovy/io/seqera/wave/util/TimedInputStream.groovy
+++ src/main/groovy/io/seqera/wave/util/TimedInputStream.groovy
@@ -19,14 +19,17 @@
 package io.seqera.wave.util
 
 import java.time.Duration
-import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Callable
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 import groovy.transform.CompileStatic
 import io.seqera.wave.core.RoutePath
 import io.seqera.wave.exception.UnexpectedReadException
-
 /**
+ * An input stream filter that implements a timeout mechanism on the reading
+ * operation over the target stream.
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
@@ -39,6 +42,8 @@ class TimedInputStream extends FilterInputStream {
 
     private final RoutePath route
 
+    private final ExecutorService executor
+
     private volatile boolean closed
 
     TimedInputStream(InputStream inputStream, Duration timeout, RoutePath route) {
@@ -46,11 +51,12 @@ class TimedInputStream extends FilterInputStream {
         this.target = inputStream
         this.timeoutMillis = (int)timeout.toMillis()
         this.route = route
+        this.executor = Executors.newVirtualThreadPerTaskExecutor()
     }
 
     @Override
     int read() throws IOException {
-        final result = CompletableFuture<Integer>.supplyAsync(() -> target.read())
+        final result = executor.submit((Callable<Integer>)(() -> target.read()))
         try {
             return result.get(timeoutMillis, TimeUnit.MILLISECONDS)
         }
@@ -62,7 +68,7 @@ class TimedInputStream extends FilterInputStream {
 
     @Override
     int read(byte[] b, int off, int len) throws IOException {
-        final result = CompletableFuture<Integer>.supplyAsync(() -> target.read(b,off,len))
+        final result = executor.submit((Callable<Integer>)(() -> target.read(b,off,len)))
         try {
             return result.get(timeoutMillis, TimeUnit.MILLISECONDS)
         }
