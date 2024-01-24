7848c817481d38864a682dd6fd815968ecff5797
Dr Marco Claudio De La Pierre <marco.delapierre@gmail.com>
2024-01-24 15:47:35 +0800

    wave spack: updated build id
    Signed-off-by: Dr Marco Claudio De La Pierre <marco.delapierre@gmail.com>

--- src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
+++ src/test/groovy/io/seqera/wave/controller/ContainerTokenControllerTest.groovy
@@ -261,12 +261,12 @@ class ContainerTokenControllerTest extends Specification {
         submit = new SubmitContainerTokenRequest(containerFile: encode('FROM foo'), spackFile: encode('some::spack-recipe'), containerPlatform: 'arm64')
         build = controller.makeBuildRequest(submit, null, "")
         then:
-        build.id == 'b7d730d274d1e057'
+        build.id == 'ac5c7bb19d6c7227'
         build.containerFile.endsWith('\nFROM foo')
         build.containerFile.startsWith('# Builder image\n')
         build.condaFile == null
         build.spackFile == 'some::spack-recipe'
-        build.targetImage == 'wave/build:b7d730d274d1e057'
+        build.targetImage == 'wave/build:ac5c7bb19d6c7227'
         build.workDir == Path.of('/some/wsp').resolve(build.id)
         build.platform == ContainerPlatform.of('arm64')
     }
