7d36c6ca4f92c3f1bf914219dd66e58486254ee8
munishchouhan <hrma017@gmail.com>
2024-02-07 16:14:27 +0100

    Metrics method added to LocalPersistenceService

--- src/main/groovy/io/seqera/wave/service/persistence/impl/LocalPersistenceService.groovy
+++ src/main/groovy/io/seqera/wave/service/persistence/impl/LocalPersistenceService.groovy
@@ -18,8 +18,11 @@
 
 package io.seqera.wave.service.persistence.impl
 
+import java.time.Instant
+
 import groovy.transform.CompileStatic
 import io.seqera.wave.core.ContainerDigestPair
+import io.seqera.wave.service.metric.Metric
 import io.seqera.wave.service.persistence.PersistenceService
 import io.seqera.wave.service.persistence.WaveBuildRecord
 import io.seqera.wave.service.persistence.WaveContainerRecord
@@ -82,4 +85,75 @@ class LocalPersistenceService implements PersistenceService {
         scanStore.get(scanId)
     }
 
+    @Override
+    Map<String, Long> getBuildCountByMetrics(Metric metric, Boolean success, Instant startDate, Instant endDate) {
+        def builds = getFilteredBuilds(success, startDate, endDate)
+        if(metric == Metric.ip)
+            return builds.groupBy { it.requestIp }
+                    .collectEntries { [it.key, it.value.size()] }
+
+        if(metric == Metric.user)
+            return builds.groupBy { it.userName}
+                    .collectEntries { [it.key, it.value.size()] }
+
+        if(metric == Metric.image)
+            return builds.groupBy { it.targetImage }
+                    .collectEntries { [it.key, it.value.size()] }
+    }
+
+    @Override
+    Long getBuildCount(Boolean success, Instant startDate, Instant endDate) {
+        def builds = getFilteredBuilds(success, startDate, endDate)
+        return builds.size()
+    }
+
+    List<WaveBuildRecord> getFilteredBuilds(Boolean success, Instant startDate, Instant endDate) {
+        def builds = buildStore.values()
+        if(startDate && endDate)
+            builds = builds.findAll { it.startTime >= startDate && it.startTime <= endDate }
+        if(success != null)
+            builds = builds.findAll { it.succeeded() == success }
+        return builds as List<WaveBuildRecord>
+    }
+
+    @Override
+    Map<String, Long> getPullCountByMetrics(Metric metric, Instant startDate, Instant endDate) {
+        def pulls = getFilteredPulls(startDate, endDate)
+        if(metric == Metric.ip)
+            return pulls.groupBy { it.ipAddress}
+                    .collectEntries { [it.key, it.value.size()] }
+        if(metric == Metric.user)
+            return pulls.groupBy { it.user.userName}
+                    .collectEntries { [it.key, it.value.size()] }
+        if(metric == Metric.image)
+            return pulls.groupBy { it.sourceImage}
+                    .collectEntries { [it.key, it.value.size()] }
+    }
+
+    @Override
+    Long getPullCount(Instant startDate, Instant endDate) {
+        def pulls = getFilteredPulls(startDate, endDate)
+        return pulls.size()
+    }
+
+    @Override
+    Long getDistinctMetrics(Metric metric, Instant startDate, Instant endDate) {
+        def pulls = getFilteredPulls(startDate, endDate)
+        if (metric == Metric.ip)
+            return pulls.collect({ it.ipAddress })
+                    .unique().size()
+        if (metric == Metric.user)
+            return pulls.collect({ it.user.userName })
+                    .unique().size()
+        if (metric == Metric.image)
+            return pulls.collect({ it.sourceImage })
+                    .unique().size()
+    }
+
+    List<WaveContainerRecord> getFilteredPulls(Instant startDate, Instant endDate) {
+        def builds = requestStore.values()
+        if(startDate && endDate)
+            builds = builds.findAll { it.timestamp >= startDate && it.timestamp <= endDate }
+        return builds as List<WaveContainerRecord>
+    }
 }
