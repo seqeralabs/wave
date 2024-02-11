e665629f518e4c6f3a6551d0f2921452d4be6135
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-02-11 18:27:10 +0100

    Fix compatibility for existing ContainerRequestData records
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/tower/PlatformId.groovy
+++ src/main/groovy/io/seqera/wave/tower/PlatformId.groovy
@@ -69,8 +69,8 @@ class PlatformId {
         return "PlatformId(" +
                 "user=" + user +
                 ", workspaceId=" + workspaceId +
-                ", accessToken='" + StringUtils.trunc(accessToken,10) + '\'' +
-                ", towerEndpoint='" + towerEndpoint + '\'' +
+                ", accessToken=" + StringUtils.trunc(accessToken,10) +
+                ", towerEndpoint=" + towerEndpoint +
                 ')';
     }
 }
