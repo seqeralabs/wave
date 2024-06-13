df8ec04177194a720a5265068d349a2aad67f5c0
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-06-13 18:47:12 +0200

    Fix get image auth digest (#530)
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/core/RegistryProxyServiceTest.groovy
+++ src/test/groovy/io/seqera/wave/core/RegistryProxyServiceTest.groovy
@@ -18,11 +18,14 @@
 
 package io.seqera.wave.core
 
+import spock.lang.Requires
 import spock.lang.Shared
 import spock.lang.Specification
 
 import io.micronaut.context.ApplicationContext
 import io.micronaut.test.extensions.spock.annotation.MicronautTest
+import io.seqera.wave.service.builder.BuildRequest
+import io.seqera.wave.tower.PlatformId
 import jakarta.inject.Inject
 /**
  *
@@ -38,25 +41,31 @@ class RegistryProxyServiceTest extends Specification {
     @Inject RegistryProxyService registryProxyService
 
 
-    def 'should check manifest exist' () {
-        given:
-        def IMAGE = 'library/hello-world:latest'
-
-        when:
-        def resp1 = registryProxyService.isManifestPresent(IMAGE)
-
-        then:
-        resp1
-    }
-
     def 'should retrieve image digest' () {
         given:
         def IMAGE = 'library/hello-world@sha256:6352af1ab4ba4b138648f8ee88e63331aae519946d3b67dae50c313c6fc8200f'
+        def request = Mock(BuildRequest)
 
         when:
-        def resp1 = registryProxyService.getImageDigest(IMAGE)
-
+        def resp1 = registryProxyService.getImageDigest(request)
+        then:
+        request.getTargetImage() >> IMAGE
         then:
         resp1 == 'sha256:6352af1ab4ba4b138648f8ee88e63331aae519946d3b67dae50c313c6fc8200f'
+    }
+
+    @Requires({System.getenv('AWS_ACCESS_KEY_ID') && System.getenv('AWS_SECRET_ACCESS_KEY')})
+    def 'should retrive image digest on ECR' () {
+        given:
+        def IMAGE = '195996028523.dkr.ecr.eu-west-1.amazonaws.com/wave/kaniko:0.1.0'
+        def request = Mock(BuildRequest)
+
+        when:
+        def resp1 = registryProxyService.getImageDigest(request)
+        then:
+        request.getTargetImage() >> IMAGE
+        request.getIdentity() >> new PlatformId()
+        then:
+        resp1 == 'sha256:05f9dc67e6ec879773de726b800d4d5044f8bd8e67da728484fbdea56af1fdff'
     }
 }
