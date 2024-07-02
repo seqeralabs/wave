2ee78a9c341bf7ed006d3e6497e8167e01c9b63a
munishchouhan <hrma017@gmail.com>
2024-07-02 15:45:21 +0200

    refactored
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/service/metric/impl/MetricsServiceImpl.groovy
+++ src/main/groovy/io/seqera/wave/service/metric/impl/MetricsServiceImpl.groovy
@@ -25,7 +25,7 @@ import java.util.regex.Pattern
 
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
-import io.seqera.wave.service.metric.MetricConstants
+import io.seqera.wave.service.metric.MetricsConstants
 import io.seqera.wave.service.metric.MetricsCounterStore
 import io.seqera.wave.service.metric.MetricsService
 import io.seqera.wave.service.metric.model.GetOrgCountResponse
@@ -52,13 +52,13 @@ class MetricsServiceImpl implements MetricsService {
     @Override
     GetOrgCountResponse getAllOrgCount(String metric){
         final response = new GetOrgCountResponse(metric, 0, [:])
-        final orgCounts = metricsCounterStore.getAllMatchingEntries("$metric/$MetricConstants.PREFIX_ORG/*")
+        final orgCounts = metricsCounterStore.getAllMatchingEntries("$metric/$MetricsConstants.PREFIX_ORG/*")
         for(def entry : orgCounts) {
             // orgCounts also contains the records with org and date, so here it filter out the records with date
-            if(!entry.key.contains("/$MetricConstants.PREFIX_DAY/")) {
+            if(!entry.key.contains("/$MetricsConstants.PREFIX_DAY/")) {
                 response.count += entry.value
                 //split is used to extract the org name from the key like "metrics/o/seqera.io" => seqera.io
-                response.orgs.put(entry.key.split("/$MetricConstants.PREFIX_ORG/").last(), entry.value)
+                response.orgs.put(entry.key.split("/$MetricsConstants.PREFIX_ORG/").last(), entry.value)
             }
         }
         return response
@@ -76,7 +76,7 @@ class MetricsServiceImpl implements MetricsService {
             response.orgs.put(org, response.count)
         }else{
             // when only date is provide, scan the store and return the  count for all orgs on given date
-            final orgCounts = metricsCounterStore.getAllMatchingEntries("$metric/$MetricConstants.PREFIX_ORG/*/$MetricConstants.PREFIX_DAY/$date")
+            final orgCounts = metricsCounterStore.getAllMatchingEntries("$metric/$MetricsConstants.PREFIX_ORG/*/$MetricsConstants.PREFIX_DAY/$date")
             for(def entry : orgCounts) {
                 response.orgs.put(extractOrgFromKey(entry.key), entry.value)
             }
@@ -88,17 +88,17 @@ class MetricsServiceImpl implements MetricsService {
 
     @Override
     void incrementFusionPullsCounter(PlatformId platformId){
-        incrementCounter(MetricConstants.PREFIX_FUSION, platformId?.user?.email)
+        incrementCounter(MetricsConstants.PREFIX_FUSION, platformId?.user?.email)
     }
 
     @Override
     void incrementBuildsCounter(PlatformId platformId){
-        incrementCounter(MetricConstants.PREFIX_BUILDS, platformId?.user?.email)
+        incrementCounter(MetricsConstants.PREFIX_BUILDS, platformId?.user?.email)
     }
 
     @Override
     void incrementPullsCounter(PlatformId platformId) {
-        incrementCounter(MetricConstants.PREFIX_PULLS, platformId?.user?.email)
+        incrementCounter(MetricsConstants.PREFIX_PULLS, platformId?.user?.email)
     }
 
     protected void incrementCounter(String prefix, String email) {
@@ -127,13 +127,13 @@ class MetricsServiceImpl implements MetricsService {
 
     protected static String getKey(String prefix, String day, String org){
         if( day && org )
-            return "$prefix/$MetricConstants.PREFIX_ORG/$org/$MetricConstants.PREFIX_DAY/$day"
+            return "$prefix/$MetricsConstants.PREFIX_ORG/$org/$MetricsConstants.PREFIX_DAY/$day"
 
         if( org )
-            return "$prefix/$MetricConstants.PREFIX_ORG/$org"
+            return "$prefix/$MetricsConstants.PREFIX_ORG/$org"
 
         if( day )
-            return "$prefix/$MetricConstants.PREFIX_DAY/$day"
+            return "$prefix/$MetricsConstants.PREFIX_DAY/$day"
 
         return null
     }
