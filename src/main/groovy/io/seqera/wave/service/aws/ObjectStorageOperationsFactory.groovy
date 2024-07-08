cb4efafd5f55597ba566421595c1e687d8ffda6a
munishchouhan <hrma017@gmail.com>
2024-07-08 16:08:47 +0200

    fixed error
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/service/aws/ObjectStorageOperationsFactory.groovy
+++ src/main/groovy/io/seqera/wave/service/aws/ObjectStorageOperationsFactory.groovy
@@ -1,3 +1,21 @@
+/*
+ *  Wave, containers provisioning service
+ *  Copyright (c) 2024, Seqera Labs
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package io.seqera.wave.service.aws
 
 import groovy.transform.CompileStatic
@@ -27,7 +45,7 @@ class ObjectStorageOperationsFactory {
     @Singleton
     @Named("build-logs")
     ObjectStorageOperations<?, ?, ?> awsStorageOperations( @Named("awsS3Client") S3Client s3Client, InputStreamMapper inputStreamMapper) {
-        AwsS3Configuration configuration = new AwsS3Configuration()
+        AwsS3Configuration configuration = new AwsS3Configuration(null)
         configuration.setBucket(storageBucket)
         return new AwsS3Operations(configuration, s3Client, inputStreamMapper)
     }
