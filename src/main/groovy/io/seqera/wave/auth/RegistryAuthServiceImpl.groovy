4c9ff0f303aab3fc12fb24dd055600ebf6d7ab2c
munishchouhan <hrma017@gmail.com>
2024-08-02 13:06:05 +0200

    minor change [ci skip]
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/auth/RegistryAuthServiceImpl.groovy
+++ src/main/groovy/io/seqera/wave/auth/RegistryAuthServiceImpl.groovy
@@ -72,12 +72,12 @@ class RegistryAuthServiceImpl implements RegistryAuthService {
             // check if there's a record in the store cache (redis)
             def result = tokenStore.get(key.toString())
             if( result ) {
-                log.debug "Authority token lookup for endpoint: '$key' => $result [from store]"
+                log.debug "Authority token lookup for cachekey: '$key' => $result [from store]"
                 return result
             }
             // look-up using the corresponding API endpoint
             result = getToken0(key)
-            log.debug "Authority token lookup for endpoint: '$key' => $result"
+            log.debug "Authority token lookup for cachekey: '$key' => $result"
             // save it in the store cache (redis)
             tokenStore.put(key.toString(), result)
             return result
