a09980cf6c1bee3fc192af541562ea70ec82e0b3
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-08-18 09:41:07 +0200

    Add trusted builds timeout (#600)
    This commit adds a the setting `wave.build.trusted-timeout` that defines the max duration for
    jobs for container build when freeze is enabled and the user identity is know.
    
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/controller/ContainerController.groovy
+++ src/main/groovy/io/seqera/wave/controller/ContainerController.groovy
@@ -329,11 +329,7 @@ class ContainerController {
         // create a unique digest to identify the build request
         final containerId = makeContainerId(containerSpec, condaContent, platform, buildRepository, req.buildContext)
         final targetImage = makeTargetImage(format, buildRepository, containerId, condaContent, nameStrategy)
-        // build max duration - when the user identity is provided and freeze is enabled
-        // use `trustedTimeout` which is expected to be longer than `defaultTimeout` `
-        final maxDuration = identity && req.freeze
-                ?  buildConfig.trustedTimeout
-                :  buildConfig.defaultTimeout
+        final maxDuration = buildConfig.buildMaxDuration(req)
 
         return new BuildRequest(
                 containerId,
