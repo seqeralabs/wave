ae16c5e88ec861794b7cf736e8c95b7ab23e6b40
munish chouhan <hrma017@gmail.com>
2024-08-22 13:30:12 +0200

    fixed compile error

--- src/main/groovy/io/seqera/wave/service/scan/ScanRequest.groovy
+++ src/main/groovy/io/seqera/wave/service/scan/ScanRequest.groovy
@@ -37,11 +37,13 @@ class ScanRequest {
     final String configJson
     final String targetImage
     final ContainerPlatform platform
+    final Path workDir
     String s3Key
 
     static ScanRequest fromBuild(BuildRequest request) {
         final id = request.scanId
+        final workDir = request.workDir.resolveSibling("scan-${id}")
         final s3Key = "workspace/scan-${id}"
-        return new ScanRequest(id, request.buildId, request.configJson, request.targetImage, request.platform, s3Key)
+        return new ScanRequest(id, request.buildId, request.configJson, request.targetImage, request.platform, workDir, s3Key)
     }
 }
