78753ede22da76f8752c05af2ed0b913af084706
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-09-05 13:03:59 +0200

    Fix failing tests
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/service/builder/ContainerBuildServiceTest.groovy
+++ src/test/groovy/io/seqera/wave/service/builder/ContainerBuildServiceTest.groovy
@@ -20,6 +20,7 @@ package io.seqera.wave.service.builder
 
 import spock.lang.Requires
 import spock.lang.Specification
+import spock.lang.Timeout
 
 import java.nio.file.Files
 import java.nio.file.Path
@@ -45,7 +46,6 @@ import io.seqera.wave.service.cleanup.CleanupStrategy
 import io.seqera.wave.service.inspect.ContainerInspectServiceImpl
 import io.seqera.wave.service.job.JobService
 import io.seqera.wave.service.job.JobSpec
-import io.seqera.wave.service.job.JobState
 import io.seqera.wave.service.persistence.PersistenceService
 import io.seqera.wave.service.persistence.WaveBuildRecord
 import io.seqera.wave.test.TestHelper
@@ -59,6 +59,7 @@ import jakarta.inject.Inject
  *
  * @author Paolo Di Tommaso <paolo.ditommaso@gmail.com>
  */
+@Timeout(60)
 @Slf4j
 @MicronautTest
 class ContainerBuildServiceTest extends Specification {
@@ -70,9 +71,9 @@ class ContainerBuildServiceTest extends Specification {
     @Inject HttpClientConfig httpClientConfig
     @Inject BuildConfig buildConfig
     @Inject BuildRecordStore buildRecordStore
+    @Inject BuildCacheStore buildCacheStore
     @Inject PersistenceService persistenceService
     @Inject JobService jobService
-
 
     @Requires({System.getenv('AWS_ACCESS_KEY_ID') && System.getenv('AWS_SECRET_ACCESS_KEY')})
     def 'should build & push container to aws' () {
@@ -100,17 +101,17 @@ class ContainerBuildServiceTest extends Specification {
                         cacheRepository: cacheRepo,
                         configJson: cfg,
                         format: BuildFormat.DOCKER,
-                        startTime: Instant.now()
+                        startTime: Instant.now(),
+                        maxDuration: Duration.ofMinutes(1)
                 )
                     .withBuildId('1')
+        and:
+        buildCacheStore.storeBuild(targetImage, new BuildStoreEntry(req, BuildResult.create(req)))
 
         when:
-        def jobSpec = service.launch(req)
-        sleep 5000 //wait for build
+        service.launch(req)
         then:
-        if(jobService.status(jobSpec).status == JobState.Status.RUNNING)
-            sleep 5000 //wait for build
-        jobService.status(jobSpec).status == JobState.Status.SUCCEEDED
+        service.buildResult(targetImage).get().succeeded()
 
         cleanup:
         folder?.deleteDir()
@@ -146,14 +147,13 @@ class ContainerBuildServiceTest extends Specification {
                         maxDuration: Duration.ofMinutes(1)
                 )
                 .withBuildId('1')
+        and:
+        buildCacheStore.storeBuild(targetImage, new BuildStoreEntry(req, BuildResult.create(req)))
 
         when:
-        def jobSpec = service.launch(req)
-        sleep 5000 //wait for build
+        service.launch(req)
         then:
-        if(jobService.status(jobSpec).status == JobState.Status.RUNNING)
-            sleep 5000 //wait for build
-        jobService.status(jobSpec).status == JobState.Status.SUCCEEDED
+        service.buildResult(targetImage).get().succeeded()
 
         cleanup:
         folder?.deleteDir()
@@ -170,7 +170,7 @@ class ContainerBuildServiceTest extends Specification {
         RUN echo Hello > hello.txt
         '''.stripIndent()
         and:
-        buildRepo = "quay.io/pditommaso/wave-tests"
+        def buildRepo = "quay.io/pditommaso/wave-tests"
         def cfg = dockerAuthService.credentialsConfigJson(dockerFile, buildRepo, null, Mock(PlatformId))
         def containerId = ContainerHelper.makeContainerId(dockerFile, null, null, ContainerPlatform.of('linux/arm64'), buildRepo, null)
         def targetImage = ContainerHelper.makeTargetImage(BuildFormat.DOCKER, buildRepo, containerId, null, null, null)
@@ -181,7 +181,7 @@ class ContainerBuildServiceTest extends Specification {
                         workspace: folder,
                         targetImage: targetImage,
                         identity: Mock(PlatformId),
-                        platform: ContainerPlatform.of('amd64'),
+                        platform: TestHelper.containerPlatform(),
                         cacheRepository: cacheRepo,
                         configJson: cfg,
                         format: BuildFormat.DOCKER,
@@ -189,14 +189,13 @@ class ContainerBuildServiceTest extends Specification {
                         maxDuration: Duration.ofMinutes(1)
                 )
                 .withBuildId('1')
+        and:
+        buildCacheStore.storeBuild(targetImage, new BuildStoreEntry(req, BuildResult.create(req)))
 
         when:
-        def jobSpec = service.launch(req)
-        sleep 5000 //wait for build
+        service.launch(req)
         then:
-        if(jobService.status(jobSpec).status == JobState.Status.RUNNING)
-            sleep 5000 //wait for build
-        jobService.status(jobSpec).status == JobState.Status.SUCCEEDED
+        service.buildResult(targetImage).get().succeeded()
 
         cleanup:
         folder?.deleteDir()
@@ -224,21 +223,21 @@ class ContainerBuildServiceTest extends Specification {
                         workspace: folder,
                         targetImage: targetImage,
                         identity: Mock(PlatformId),
-                        platform: ContainerPlatform.of('amd64'),
+                        platform: TestHelper.containerPlatform(),
                         cacheRepository: cacheRepo,
                         configJson: cfg,
                         format: BuildFormat.DOCKER,
-                        startTime: Instant.now()
+                        startTime: Instant.now(),
+                        maxDuration: Duration.ofMinutes(1)
                 )
                 .withBuildId('1')
+        and:
+        buildCacheStore.storeBuild(targetImage, new BuildStoreEntry(req, BuildResult.create(req)))
 
         when:
-        def jobSpec = service.launch(req)
-        sleep 5000 //wait for build
+        service.launch(req)
         then:
-        if(jobService.status(jobSpec).status == JobState.Status.RUNNING)
-            sleep 5000 //wait for build
-        jobService.status(jobSpec).status == JobState.Status.SUCCEEDED
+        service.buildResult(targetImage).get().succeeded()
 
         cleanup:
         folder?.deleteDir()
@@ -282,10 +281,11 @@ class ContainerBuildServiceTest extends Specification {
                         workspace: folder,
                         targetImage: targetImage,
                         identity: Mock(PlatformId),
-                        platform: ContainerPlatform.of('amd64'),
+                        platform: TestHelper.containerPlatform(),
                         cacheRepository: cacheRepo,
                         format: BuildFormat.DOCKER,
-                        startTime: Instant.now()
+                        startTime: Instant.now(),
+                        maxDuration: Duration.ofMinutes(1)
                 )
                 .withBuildId('1')
         and:
@@ -475,13 +475,13 @@ class ContainerBuildServiceTest extends Specification {
         '''.stripIndent()
 
     }
+
     @Requires({System.getenv('DOCKER_USER') && System.getenv('DOCKER_PAT')})
     def 'should build & push container to docker.io with local layers' () {
         given:
         def folder = Files.createTempDirectory('test')
         def buildRepo = "docker.io/pditommaso/wave-tests"
         def cacheRepo = buildConfig.defaultCacheRepository
-        def context = Files.createDirectories(folder.resolve('context'))
         def layer = Files.createDirectories(folder.resolve('layer'))
         def file1 = layer.resolve('hola.txt'); file1.text = 'Hola\n'
         def file2 = layer.resolve('ciao.txt'); file2.text = 'Ciao\n'
@@ -509,17 +509,17 @@ class ContainerBuildServiceTest extends Specification {
                         configJson: cfg,
                         containerConfig: containerConfig ,
                         format: BuildFormat.DOCKER,
-                        startTime: Instant.now()
+                        startTime: Instant.now(),
+                        maxDuration: Duration.ofMinutes(1)
                 )
                         .withBuildId('1')
-
+        and:
+        buildCacheStore.storeBuild(targetImage, new BuildStoreEntry(req, BuildResult.create(req)))
+        
         when:
-        def jobSpec = service.launch(req)
-        sleep 5000 //wait for build
+        service.launch(req)
         then:
-        if(jobService.status(jobSpec).status == JobState.Status.RUNNING)
-            sleep 5000 //wait for build
-        jobService.status(jobSpec).status == JobState.Status.SUCCEEDED
+        service.buildResult(targetImage).get().succeeded()
 
         cleanup:
         folder?.deleteDir()
