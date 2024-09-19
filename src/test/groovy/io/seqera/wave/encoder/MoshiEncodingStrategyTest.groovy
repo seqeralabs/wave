f06beaa12a3096ecf79d64ef01d1242ae5ac39eb
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-09-19 12:14:01 +0200

    Add legacy test
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/encoder/MoshiEncodingStrategyTest.groovy
+++ src/test/groovy/io/seqera/wave/encoder/MoshiEncodingStrategyTest.groovy
@@ -355,6 +355,21 @@ class MoshiEncodingStrategyTest extends Specification {
         copy == record1
     }
 
+    def 'should decode legacy record with spack config' () {
+        given:
+        def legacy = '{"buildId":"12345_1","condaFile":"conda spec","dockerFile":"from foo","duration":"3000000000","exitStatus":-1,"platform":"linux/amd64","requestIp":"1.2.3.4","scanId":"scan12345","spackFile":"spack spec","targetImage":"docker.io/some:image:12345"}\n'
+        and:
+        def encoder = new MoshiEncodeStrategy<WaveBuildRecord>() { }
+
+        // verify decoding is OK when the payload contains `spackFile` not existing anymore in the WaveBuildRecord
+        when:
+        def rec = encoder.decode(legacy)
+        then:
+        rec.buildId == '12345_1'
+        rec.condaFile == 'conda spec'
+        rec.dockerFile == 'from foo'
+    }
+
     def 'should encode and decode registry info' () {
         given:
         def encoder = new MoshiEncodeStrategy<RegistryAuth>() { }
