56450fd48d0140cc2f649d4560d1656abf1a0687
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-09-25 17:36:10 +0200

    Rename Redis package
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/redis/RedisFactory.groovy
+++ src/main/groovy/io/seqera/wave/redis/RedisFactory.groovy
@@ -16,7 +16,7 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package io.seqera.wave.service.cache.impl
+package io.seqera.wave.redis
 
 
 import groovy.transform.CompileStatic
