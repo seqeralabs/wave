7276bf7f084d93c854238a6dcf98c8e3eaa86699
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2024-10-05 22:24:38 +0200

    Fix failing tests
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/test/groovy/io/seqera/wave/util/ContainerHelperTest.groovy
+++ src/test/groovy/io/seqera/wave/util/ContainerHelperTest.groovy
@@ -52,7 +52,7 @@ class ContainerHelperTest extends Specification {
         then:
         result =='''\
                 BootStrap: docker
-                From: mambaorg/micromamba:1.5.8-lunar
+                From: mambaorg/micromamba:1.5.10-lunar
                 %post
                     micromamba install -y -n base -c conda-forge -c defaults -f https://foo.com/lock.yml
                     micromamba install -y -n base foo::one bar::two
@@ -75,7 +75,7 @@ class ContainerHelperTest extends Specification {
 
         then:
         result =='''\
-                FROM mambaorg/micromamba:1.5.8-lunar
+                FROM mambaorg/micromamba:1.5.10-lunar
                 RUN \\
                     micromamba install -y -n base -c conda-forge -c defaults -f https://foo.com/lock.yml \\
                     && micromamba install -y -n base foo::one bar::two \\
@@ -99,7 +99,7 @@ class ContainerHelperTest extends Specification {
         then:
         result =='''\
                 BootStrap: docker
-                From: mambaorg/micromamba:1.5.8-lunar
+                From: mambaorg/micromamba:1.5.10-lunar
                 %files
                     {{wave_context_dir}}/conda.yml /scratch/conda.yml
                 %post
@@ -123,7 +123,7 @@ class ContainerHelperTest extends Specification {
 
         then:
         result =='''\
-                FROM mambaorg/micromamba:1.5.8-lunar
+                FROM mambaorg/micromamba:1.5.10-lunar
                 COPY --chown=$MAMBA_USER:$MAMBA_USER conda.yml /tmp/conda.yml
                 RUN micromamba install -y -n base -f /tmp/conda.yml \\
                     && micromamba install -y -n base foo::one bar::two \\
