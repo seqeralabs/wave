7490150cfa8d77f6b28d9000d534e44bf7eb2546
munishchouhan <hrma017@gmail.com>
2024-10-29 13:23:48 +0100

    formatted [ci skip]
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/controller/ViewController.groovy
+++ src/main/groovy/io/seqera/wave/controller/ViewController.groovy
@@ -233,8 +233,9 @@ class ViewController {
         }
         //add conda lock file when available
         if( buildLogService && result.condaFile ) {
-             def condaLock = buildLogService.fetchCondaLockString(result.buildId)
+            def condaLock = buildLogService.fetchCondaLockString(result.buildId)
             def buildId = result.buildId
+
             if ( condaLock && condaLock.contains('cat environment.lock')) {
                 condaLock = null //if there is no conda lock file, don't show the conda lock data
                 def builds = persistenceService.allBuilds(result.buildId.split('-')[1].split('_')[0])
@@ -245,6 +246,7 @@ class ViewController {
                     }
                 }
             }
+
             binding.build_conda_lock_data = condaLock
             binding.build_conda_lock_url = "$serverUrl/v1alpha1/builds/${buildId}/condalock"
         }
