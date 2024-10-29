22a5af7f4b334a2f0b48a130c09c87808082a849
munishchouhan <hrma017@gmail.com>
2024-10-29 13:21:54 +0100

    fix lockfile download
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/controller/ViewController.groovy
+++ src/main/groovy/io/seqera/wave/controller/ViewController.groovy
@@ -234,17 +234,19 @@ class ViewController {
         //add conda lock file when available
         if( buildLogService && result.condaFile ) {
              def condaLock = buildLogService.fetchCondaLockString(result.buildId)
+            def buildId = result.buildId
             if ( condaLock && condaLock.contains('cat environment.lock')) {
                 condaLock = null //if there is no conda lock file, don't show the conda lock data
                 def builds = persistenceService.allBuilds(result.buildId.split('-')[1].split('_')[0])
                 for (def build : builds) {
                     if (build.succeeded()){
+                        buildId = build.buildId
                         condaLock = buildLogService.fetchCondaLockString(build.buildId)
                     }
                 }
             }
             binding.build_conda_lock_data = condaLock
-            binding.build_conda_lock_url = "$serverUrl/v1alpha1/builds/${result.buildId}/condalock"
+            binding.build_conda_lock_url = "$serverUrl/v1alpha1/builds/${buildId}/condalock"
         }
         // result the main object
         return binding
