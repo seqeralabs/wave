39db05c5f729b2ef0584b07bb16bb10a9b3d4aaa
munishchouhan <hrma017@gmail.com>
2025-01-24 15:10:32 +0100

    added test
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/test/groovy/io/seqera/wave/controller/OpenAPIControllerTest.groovy
+++ src/test/groovy/io/seqera/wave/controller/OpenAPIControllerTest.groovy
@@ -18,24 +18,24 @@
 
 package io.seqera.wave.controller
 
-import groovy.transform.CompileStatic
+import spock.lang.Specification
+
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Get
-import io.micronaut.scheduling.TaskExecutors
-import io.micronaut.scheduling.annotation.ExecuteOn
+import io.micronaut.http.HttpStatus
 /**
- * Implements a controller for openapi
  *
  * @author Munish Chouhan <munish.chouhan@seqera.io>
  */
-@CompileStatic
-@Controller
-@ExecuteOn(TaskExecutors.BLOCKING)
-class OpenAPIController {
-    @Get(uri = "/openapi")
-    HttpResponse getOpenAPI() {
-    println("Redirecting to /openapi/")
-        HttpResponse.redirect(URI.create("/openapi/"))
+class OpenAPIControllerTest extends Specification {
+    def 'should redirect to /openapi/'() {
+        given:
+        OpenAPIController controller = new OpenAPIController()
+
+        when:
+        HttpResponse response = controller.getOpenAPI()
+
+        then:
+        response.status == HttpStatus.MOVED_PERMANENTLY
+        response.header('Location') == '/openapi/'
     }
 }
