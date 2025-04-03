553273e9e264972eda550227a654302cec2d8f6e
munishchouhan <hrma017@gmail.com>
2025-04-03 15:08:13 +0200

    [release] test 1.19.5-A0
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/service/k8s/K8sServiceImpl.groovy
+++ src/main/groovy/io/seqera/wave/service/k8s/K8sServiceImpl.groovy
@@ -329,7 +329,7 @@ class K8sServiceImpl implements K8sService {
             container
             // use 'command' to override the entrypoint of the container
                     .withCommand(args)
-                    .withNewSecurityContext().withPrivileged(true).endSecurityContext()
+                    .withNewSecurityContext().withPrivileged(false).endSecurityContext()
         } else {
             container
                     //required by buildkit rootless container
@@ -601,7 +601,7 @@ class K8sServiceImpl implements K8sService {
             container
             // use 'command' to override the entrypoint of the container
                     .withCommand(args)
-                    .withNewSecurityContext().withPrivileged(true).endSecurityContext()
+                    .withNewSecurityContext().withPrivileged(false).endSecurityContext()
         } else {
             container
             //required by buildkit rootless container
