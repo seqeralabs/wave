973530961ea1f703d943cb2f8cdbd0897587da92
Paolo Di Tommaso <paolo.ditommaso@gmail.com>
2025-05-28 16:32:25 +0200

    Add annotation
    Signed-off-by: Paolo Di Tommaso <paolo.ditommaso@gmail.com>

--- src/main/groovy/io/seqera/wave/util/FusionVersionStringDeserializer.groovy
+++ src/main/groovy/io/seqera/wave/util/FusionVersionStringDeserializer.groovy
@@ -22,11 +22,16 @@ import com.fasterxml.jackson.core.JsonParser
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JsonDeserializer
 import com.fasterxml.jackson.databind.JsonNode
+import groovy.transform.CompileStatic
+import io.seqera.wave.service.persistence.migrate.MigrationOnly
+
 /**
  * Custom deserializer for Fusion version strings.
  *
  * @author Munish Chouhan <munish.chouhan@seqera.io>
  */
+@MigrationOnly
+@CompileStatic
 class FusionVersionStringDeserializer extends JsonDeserializer<String> {
     @Override
     String deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
