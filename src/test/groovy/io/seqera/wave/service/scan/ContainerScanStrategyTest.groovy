80fb242077833d1462887274f68147e1348f0724
munishchouhan <hrma017@gmail.com>
2025-08-11 15:00:11 +0200

    fixed tests
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/test/groovy/io/seqera/wave/service/scan/ContainerScanStrategyTest.groovy
+++ src/test/groovy/io/seqera/wave/service/scan/ContainerScanStrategyTest.groovy
@@ -55,7 +55,7 @@ class ContainerScanStrategyTest extends Specification {
                      '--output',
                      '/work/dir/report.json',
                      '--cache-dir',
-                     '/work/dir/trivy-cache',
+                     '/tmp/trivy-cache',
                      targetImage]
     }
 
@@ -85,7 +85,7 @@ class ContainerScanStrategyTest extends Specification {
                      '--output',
                      '/work/dir/report.json',
                      '--cache-dir',
-                     '/work/dir/trivy-cache',
+                     '/tmp/trivy-cache',
                      '--severity',
                      'low,high',
                      targetImage]
@@ -117,7 +117,7 @@ class ContainerScanStrategyTest extends Specification {
                      '--output',
                      '/work/dir/spdx.json',
                      '--cache-dir',
-                     '/work/dir/trivy-cache',
+                     '/tmp/trivy-cache',
                      targetImage]
     }
 
@@ -135,6 +135,6 @@ class ContainerScanStrategyTest extends Specification {
         when:
         def command = containerScanStrategy.trivyCommand(targetImage, workDir, platform, config)
         then:
-        command == [ 'trivy --quiet image --platform linux/amd64 --timeout 100m --format json --output /work/dir/report.json --cache-dir /work/dir/trivy-cache --severity low,high repository/scantool && trivy --quiet image --platform linux/amd64 --timeout 100m --format spdx-json --output /work/dir/spdx.json --cache-dir /work/dir/trivy-cache repository/scantool' ]
+        command == [ 'trivy --quiet image --platform linux/amd64 --timeout 100m --format json --output /work/dir/report.json --cache-dir /tmp/trivy-cache --severity low,high repository/scantool && trivy --quiet image --platform linux/amd64 --timeout 100m --format spdx-json --output /work/dir/spdx.json --cache-dir /tmp/trivy-cache repository/scantool' ]
     }
 }
