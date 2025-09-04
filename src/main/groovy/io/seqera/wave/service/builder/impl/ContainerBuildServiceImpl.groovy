1193c6910f72f66d00d8db0bd63e367dfd23f3c3
munishchouhan <hrma017@gmail.com>
2025-09-04 17:26:50 +0200

    [release] test 1.25.1-A4
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/service/builder/impl/ContainerBuildServiceImpl.groovy
+++ src/main/groovy/io/seqera/wave/service/builder/impl/ContainerBuildServiceImpl.groovy
@@ -72,8 +72,6 @@ import static io.seqera.wave.util.RegHelper.layerName
 import static java.nio.file.StandardOpenOption.CREATE
 import static java.nio.file.StandardOpenOption.TRUNCATE_EXISTING
 import static java.nio.file.StandardOpenOption.WRITE
-import static java.nio.file.attribute.PosixFilePermission.OWNER_READ
-import static java.nio.file.attribute.PosixFilePermission.OWNER_WRITE
 /**
  * Implements container build service
  *
@@ -203,9 +201,6 @@ class ContainerBuildServiceImpl implements ContainerBuildService, JobHandler<Bui
                 final remoteFile = req.workDir.resolve('singularity-remote.yaml')
                 final content = RegHelper.singularityRemoteFile(req.targetImage)
                 Files.write(remoteFile, content.bytes, CREATE, WRITE, TRUNCATE_EXISTING)
-                // set permissions 600 as required by Singularity
-                Files.setPosixFilePermissions(configFile, Set.of(OWNER_READ, OWNER_WRITE))
-                Files.setPosixFilePermissions(remoteFile, Set.of(OWNER_READ, OWNER_WRITE))
             }
             // save layers provided via the container config
             if( req.containerConfig ) {
