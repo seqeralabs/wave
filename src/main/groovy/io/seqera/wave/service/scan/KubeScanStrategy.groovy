5e2b0da322e8e01b756ba908ed6930504aee8d52
munishchouhan <hrma017@gmail.com>
2025-11-18 13:33:57 +0100

    fixed launchScanJob
    Signed-off-by: munishchouhan <hrma017@gmail.com>

--- src/main/groovy/io/seqera/wave/service/scan/KubeScanStrategy.groovy
+++ src/main/groovy/io/seqera/wave/service/scan/KubeScanStrategy.groovy
@@ -71,7 +71,8 @@ class KubeScanStrategy extends ScanStrategy {
 
             // Use unified scan image and command for both container and plugin scans
             final command = buildScanCommand(entry.containerImage, entry.workDir, entry.platform, scanConfig)
-            k8sService.launchScanJob(jobName, scanConfig.scanImage, command, entry.workDir, configFile, scanConfig)
+            final selector = getSelectorLabel(entry.platform, nodeSelectorMap)
+            k8sService.launchScanJob(jobName, scanConfig.scanImage, command, entry.workDir, configFile, scanConfig, selector)
         }
         catch (ApiException e) {
             throw new BadRequestException("Unexpected scan failure: ${e.responseBody}", e)
